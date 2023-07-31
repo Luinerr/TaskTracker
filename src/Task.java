a4ce4a8b2b6b5a86cf2b716c1ef76dcb8d868e76
Ivan <zivan2000@mail.ru>
2023-07-31 14:45:30 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+
+    String name;
+    String details;
+    int id;
+    int status;
+
+    public Task(String name, String details, int id, int status) {
+        this.name = name;
+        this.details = details;
+        this.id = id;
+        this.status = status;
+    }
+
+    
+}
